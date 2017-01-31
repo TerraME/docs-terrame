da4fbe36ade2ed9d17ef37b9636f9051ce4b5629
Pedro Andrade <pedro.andrade@inpe.br>
2017-01-31 10:56:14 -0200

    Removing some unnecessary files.

--- CreatingAndFillingCellularSpaces/itaituba/description.lua
+++
@@ -1,8 +0,0 @@
-version = "0.1"
-license = "LGPL-3"
-package = "itaituba"
-title = ""
-url = "http://www.terrame.org"
-authors = "INPE"
-contact = "pedro.andrade@inpe.br"
-content = [[This package is a small example about how to automatically document cellular spaces.]]
