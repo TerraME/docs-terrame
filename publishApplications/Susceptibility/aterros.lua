030259f532146d55d64ce3c590fb80ecf1e3eebc
Pedro Andrade <pedro.andrade@inpe.br>
2017-05-09 14:06:22 -0300

    Susceptibility application added.

---
+++ publishApplications/Susceptibility/aterros.lua
@@ -0,0 +1,39 @@
+
+import("terralib")
+
+project = Project{
+	file = "aterros.tview",
+	clean = true,
+	aterros = "aterros_c.shp",
+-- so de incluir o layer abaixo o Application para de funcionar!!
+--	resultado = "Resultado_Modelo_SP.shp"
+}
+
+import("publish")
+
+Application{
+    project = "aterros.tview",
+	base = "terrain",
+    title = "Susceptibilidade de aterros",
+    description = "Resultados da tese de doutorado entitulada 'Modeling environmental susceptibility of municipal solid waste disposal sites in regional scale'.",
+    clean = true,
+    output = "VictorWebMap",
+    aterros = View{
+		select = {"OBJECTID", "icon"},
+		icon = {"home", "forest"},
+        description = "Aterros",
+		report = function(cell)
+			local mreport = Report{
+				title = cell.Municipio,
+				author = "Nascimento (2017)"
+			}
+
+			mreport:addText("F6: "..cell.F6)
+			mreport:addText("Fluxo: "..cell.RSUdia14.."ton/dia")
+			mreport:addText("Area: "..math.floor(cell.Area_ha).."ha")
+
+			return mreport
+		end
+    },
+}
+
