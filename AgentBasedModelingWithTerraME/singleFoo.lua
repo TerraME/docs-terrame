36f269f538d8dae69f3eed0261ca7e0f3d675c72
Pedro Andrade <pedro.andrade@inpe.br>
2017-06-08 15:05:14 -0300

    First result of ABM tutorial.

---
+++ AgentBasedModelingWithTerraME/singleFoo.lua
@@ -0,0 +1,38 @@
+singleFooAgent = Agent{
+    size = 10,
+    name = "foo",
+    execute = function(self)
+        self.size = self.size + 1
+        self:walk()
+    end
+}
+
+cs = CellularSpace{
+    xdim = 10
+}
+
+cs:createNeighborhood{} -- Moore neighborhood as default
+
+e = Environment{
+    cs,
+    singleFooAgent
+}
+
+e:createPlacement{} -- random
+
+map = Map{
+    target = cs,
+    grouping = "placement",
+	value = {0, 1},
+	color = {"gray", "blue"}
+}
+
+t = Timer{
+    Event{action = singleFooAgent},
+    Event{action = map}
+}
+
+t:run(100)
+
+map:save("singleFoo.png")
+
