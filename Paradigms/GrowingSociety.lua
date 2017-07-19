6801092b9bf52693698ce9f30330db8ec1de97f9
Pedro Andrade <pedro.andrade@inpe.br>
2017-07-19 16:21:15 -0300

    Updates in Paradigms tutorial related to RC-4.

--- Paradigms/GrowingSociety.lua
+++ Paradigms/GrowingSociety.lua
@@ -5,16 +5,16 @@ GrowingSociety = Model{
     init = function(model)
         model.agent = Agent{
             execute = function(self)
-				local cell = self:getCell():getNeighborhood():sample()
-                if cell:isEmpty() and Random{p = 0.3}:sample() then
+				local cell = self:emptyNeighbor()
+                if cell and Random{p = 0.3}:sample() then
                     local child = self:reproduce()
                     child:move(cell)
                 end
 
-				cell = self:getCell():getNeighborhood():sample()
-				if cell:isEmpty() then
-					self:move(cell)
-				end
+                cell = self:emptyNeighbor()
+                if cell then
+                    self:move(cell)
+                end
             end
         }
 
