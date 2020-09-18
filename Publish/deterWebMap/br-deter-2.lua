69b9675ebee0de2357f706e9db180101c1ef93a2
Pedro R. Andrade <pedro.andrade.inpe@gmail.com>
2020-09-18 09:23:56 -0300

    Updates to DETER application.

--- Publish/deterWebMap/br-deter-2.lua
+++ Publish/deterWebMap/br-deter-2.lua
@@ -11,15 +11,34 @@ Application{
  	key = "AIzaSyA1coAth-Bo7m99rnxOm2oOBB88AmaSbOk",
 	project = "amaz_final.tview",
 	base = "roadmap",
-	title	= "DETER acumulado",
-	description = "Esta aplicação mostra os dados do DETER acumulados para o ano de 2020, em uma resolução de 150x150km.",
+	title	= "DETER celular mensal",
+	description = "Esta aplicação mostra os dados do DETER acumulados por mês em uma representação celular com resolução de 150x150km, para o ano de 2020.",
 	output	= "deterWebMap",
-
+    --[[
+    aml = View{
+		description = "Legal Amazon area. This area was initially established by "..link("http://www2.camara.leg.br/legin/fed/lei/1950-1959/lei-1806-6-janeiro-1953-367342-publicacaooriginal-1-pl.html", "Brazilian Law 1806")..". It covers all the states from the North region in Brazil, plus Mato Grosso and part of Maranhao. PRODES monitors deforestation within this area. It contains almost all Amazon biome, except by part of Maranhao state. The areas in Legal Amazon outside Amazon biome belong to Cerrado biome.",
+		title = "Legal Amazon",
+		color = "white",
+		transparency = 1,
+		visible = false
+	},
+    --]]
+    --[[
+    	bioma = View{
+		description = "Brazilian Amazon biome. It covers approximately half the area of Brazil. The definition of Amazon biome is officially provided by "..link("https://ww2.ibge.gov.br/home/presidencia/noticias/21052004biomashtml.shtm", "IBGE")..".",
+		title = "Amazon Biome",
+		color = "green",
+		transparency = 0.6,
+		visible = false
+	},
+--]]
 	deter = View{
-        value = {"low", "medium", "high"},
-        color = {"blue", "yellow", "red"},
+        value = {"1muitobaixo", "2baixo", "3medio", "4alto", "5muitoalto"},
+--        label = {"Muito Baixo", "Baixo", "Medio", "Alto", "Muito Alto"}, -- BUG: ele reordena a legenda alfabeticamente
+        color = {"green", "greenyellow", "yellow", "orange", "red"},
 		select  = "status",
 		time = "snapshot",
-		description = "DETER acumulado 2020",
+        transparency = 0.2,
+		description = "DETER mensal 2020",
 	},
 }
